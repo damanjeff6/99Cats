172e5dfc12e68a591bc6ac1798706c71f0fd6a22
App Academy Student <student@appacademy.io>
2013-12-11 13:40:45 -0800

    initial setup and createcats migration

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ninetyninecats::Application.config.secret_token = '279ee307a8babb88de604f6b266ea799024c38735772f5a2810af11c0de1a784897d515997c9e497f39d33b6a2ba93f87eda32ed8a099f4001e7b96ac247f03f'
